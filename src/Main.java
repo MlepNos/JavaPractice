6a68631bfeb818c927c162af4754d28a64135d6f
MlepNos <72391873+MlepNos@users.noreply.github.com>
2023-05-08 18:05:43 +0200

    question3

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        printYearsAndDays(525600);
-        printYearsAndDays(1051200);
-        printYearsAndDays(561600);
-        printYearsAndDays(0);
+
 
 
     }
@@ -59,9 +56,30 @@ printYearsAndDays(561600);  → should print "561600 min = 1 y and 25 d"
 
         System.out.println(minutes + " min = " + year +" y and "+ DD +" d");}
     }
+/* 3_Equality Printer
+    Write a method printEqual with 3 parameters of type int. The method should not return anything .
+    If one of the parameters is less than 0, print text "Invalid Value".If all numbers are equal print text "All numbers are equal"
+    If all numbers are different print text "All numbers are different". Otherwise, print "Neither all are equal or different".
+
+    EXAMPLES OF INPUT/OUTPUT:
+    printEqual(1, 1, 1); should print text All numbers are equal
+    printEqual(1, 1, 2); should print text Neither all are equal or different
+    printEqual(-1, -1, -1); should print text Invalid Value
+    printEqual(1, 2, 3); should print text All numbers are different
+*/
+public static void printEqual(int a, int b, int c){
+    if(a<0 || b<0 || c<0){
+        System.out.println("Invalid Value");
+    }else if(a==b && b==c){
+        System.out.println("All numbers are equal");
+    }else if(a != b && a != c && b != c){
+        System.out.println("All numbers are different");
+    }else{
+        System.out.println("Neither all are equal or different");
+    }
 
 
-
+}
 
 
 
