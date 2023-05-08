5d122aa76f0dfcf7c0bcc38c315da08ddd417cee
MlepNos <72391873+MlepNos@users.noreply.github.com>
2023-05-08 17:56:33 +0200

    question2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+        printYearsAndDays(0);
 
 
-        }
+    }
 
         /* 1_Area Calculator
 Write a method named area with one double parameter named radius.The method needs to return a double value that represents the area of a circle.
@@ -24,4 +28,52 @@ For formulas and PI value please check the tips below.
             }
             return x*y;
         }
+
+
+        /* 2_Minutes To Years and Days Calculator
+Write a method printYearsAndDays with parameter of type long named minutes.The method should not return anything ,and
+it needs to calculate the years and days from the minutes parameter.If the parameter is less than 0, print text "Invalid Value".
+Otherwise, if the parameter is valid then it needs to print a message in the format "XX min = YY y and ZZ d".
+XX represents the original value minutes.
+YY represents the calculated years.
+ZZ represents the calculated days.
+
+EXAMPLES OF INPUT/OUTPUT:
+printYearsAndDays(525600);  → should print "525600 min = 1 y and 0 d"
+printYearsAndDays(1051200); → should print "1051200 min = 2 y and 0 d"
+printYearsAndDays(561600);  → should print "561600 min = 1 y and 25 d"
+1 hour = 60 minutes
+1 day = 24 hours
+1 year = 365 days
+
+*/
+
+    public static void printYearsAndDays(long minutes){
+        if(minutes < 0){
+            System.out.println("Invalid Value");
+        }else {
+        long hours = minutes / 60;
+        long day = hours / 24;
+        long year = day / 365;
+        long DD = day % 365;
+
+        System.out.println(minutes + " min = " + year +" y and "+ DD +" d");}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
