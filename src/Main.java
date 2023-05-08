cbc9eab9ae9e2a39b39ee801d27560bcaf304861
MlepNos <72391873+MlepNos@users.noreply.github.com>
2023-05-08 18:51:05 +0200

    question4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,16 @@
 public class Main {
     public static void main(String[] args) {
 
+            int num= 1023;
+
+
+            while(num > 0) //do till num greater than  0
+            {
+                int mod = num % 10;
+                System.out.println(mod);
+                num = num / 10;
+            }
+
 
 
     }
@@ -77,9 +87,35 @@ public static void printEqual(int a, int b, int c){
     }else{
         System.out.println("Neither all are equal or different");
     }
+}
+
+/* 4_Playing Cat
+The cats spend most of the day playing. In particular, they play if the temperature is between 25 and 35 (inclusive).
+Unless it is summer, then the upper limit is 45 (inclusive) instead of 35.Write a method isCatPlaying that has 2 parameters.
+Method needs to return true if the cat is playing, otherwise return false
+1st parameter should be of type boolean and be named summer it represents if it is summer.
+2nd parameter represents the temperature and is of type int with the name temperature.
+
+EXAMPLES OF INPUT/OUTPUT:
+isCatPlaying(true, 10); should return false since temperature is not in range 25 - 45
+isCatPlaying(false, 36); should return false since temperature is not in range 25 - 35 (summer parameter is false)
+isCatPlaying(false, 35); should return true since temperature is in range 25 - 35  */
+
+public static boolean isCatPlaying(boolean summer, int temperature){
+    if(summer && temperature <= 45 && temperature >=25){
+        return true;
+    }else if(!summer && temperature <= 35 && temperature >=25){
+        return true;
+    }else {
+        return false;
+    }
+
+
 
 
 }
+
+
 
 
 
