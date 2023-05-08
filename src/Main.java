27f9302cd85c25ca2b423b4c0808bac671c29af6
MlepNos <72391873+MlepNos@users.noreply.github.com>
2023-05-08 17:25:29 +0200

    question1

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,26 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            System.out.println("kaan fasfasfa");
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            //System.out.println("i = " + i);
         }
-    }
+
+        /* 1_Area Calculator
+Write a method named area with one double parameter named radius.The method needs to return a double value that represents the area of a circle.
+If the parameter radius is negative then return -1.0 to represent an invalid value.Write another overloaded method with 2 parameters x and y (both doubles), where x and y represent the sides of a rectangle.
+The method needs to return an area of a rectangle.If either or both parameters is/are a negative return -1.0 to indicate an invalid value.
+For formulas and PI value please check the tips below.
+*/
+        public static double area(double radius){
+            if(radius < 0){
+                return -1;
+            }
+            return Math.PI * Math.pow(radius,2);
+        }
+        public static double area(double x,double y){
+            if(x<0 || y<0){
+                return -1;
+            }
+            return x*y;
+        }
 }
